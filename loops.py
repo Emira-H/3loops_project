f88543ffa5a86994d9fc9c3c8ca2c13c49e22a4d
michel <adolfo.ramirez1939@gmail.com>
2019-10-21 15:00:12 +0200

    fixed Steve loop

--- loops.py
+++ loops.py
@@ -1,9 +1,11 @@
 theString = "je suis - pas +"
 
+print(theString)
 if "-" in theString:
     i=0
     while "-" in theString:
         if theString[i] == '-':
             break
         i+=1
-        theString = theString[:i]+'\_'+theString[i+2:
+        theString = theString[:i]+'\_'+theString[i+2]
+print(theString)
